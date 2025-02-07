a31308c3d92c6ac17b503bc456abe19fdd9fa7ef
PawelTkocz <pawel.tkocz@vp.pl>
2025-02-07 23:33:51 +0100

    refactor: introduce intersectionCarInfo typedDict

--- animations/intersection/Intersection.py
+++ animations/intersection/Intersection.py
@@ -1,11 +1,14 @@
-import math
-
-from Geometry import Direction, Directions, Point, Rectangle, tuples_list
-from animations.constants import SAVED_CAR_MOVEMENT_DIRECTORY
+from typing import TypedDict
+from Geometry import Directions, Rectangle
+from animations.constants import (
+    SAVED_CAR_MOVEMENT_DIRECTORY as general_car_movement_directory,
+)
+from animations.intersection.constants import (
+    SAVED_CAR_MOVEMENT_DIRECTORY as intersection_car_movement_directory,
+)
 from animations.intersection.IntersectionManoeuvre import IntersectionManoeuvre
 from animations.intersection.Manoeuvre import Manoeuvre
 from animations.intersection.StreetIntersection import StreetIntersection
-from animations.intersection.constants import ROAD_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH
 from autonomousDriving.BasicAutonomousDriving import (
     BasicAutonomousDriving,
 )
@@ -13,22 +16,28 @@ from cars.BasicBrand import BasicBrand
 from cars.Car import Car, SpeedModifications
 import os
 
-import pygame
+
+class IntersectionCarInfo(TypedDict):
+    id: int  # car id
+    car: Car  # car instance
+    autonomous_driving: BasicAutonomousDriving  # autonomous driving instance
+    starting_site: (
+        Directions  # site of the intersection where the car starts its movement
+    )
+    ending_site: Directions  # site of the intersection where the car ends its movement
+    sites_with_preference: list[
+        Directions
+    ]  # list of intersection sites which have preference during this manoeuvre
+    cars_with_preference: list[
+        "IntersectionCarInfo"
+    ]  # list of cars which have preference during this manoeuvre
+    movement_history: list[tuple[Directions, SpeedModifications]]  # movement history
 
 
-# if it will still be lagging i can do sth like computation of max velocities for each point on the line before the animation starts
 class Intersection:
-    saved_car_movement_directory = "Intersection"
-
     def __init__(self, read_movement_from_file: bool, directory_name: str):
-        self.cars = []
-        self.starting_site = []
-        self.ending_site = []
-        self.preferences_directions = []
-        self.preferences_cars = []
-        self.autonomous_drivings = []
-        self.movement_histories = []
-        self.steps = []
+
+        self.cars_info: list[IntersectionCarInfo] = []
         self.read_movement_from_file = read_movement_from_file
         self.directory_name = directory_name
         self.street_intersection = StreetIntersection()
@@ -36,36 +45,37 @@ class Intersection:
 
     def draw(self, screen):
         self.street_intersection.draw(screen)
-        for car in self.cars:
+        for car in [car_info["car"] for car_info in self.cars_info]:
             car.draw(screen)
 
     def move(self):
         if self.read_movement_from_file:
-            for index, autonomous_driving in enumerate(self.autonomous_drivings):
-                if self.counter >= len(self.steps[index]):
+            for car_info in self.cars_info:
+                if self.counter >= len(car_info["movement_history"]):
                     return
-                autonomous_driving.apply_best_changes(
-                    Directions[self.steps[index][self.counter][0]],
-                    SpeedModifications[self.steps[index][self.counter][1]],
+                car_info["autonomous_driving"].apply_best_changes(
+                    Directions[car_info["movement_history"][self.counter][0]],
+                    SpeedModifications[car_info["movement_history"][self.counter][1]],
                 )
             self.counter += 1
             return
-        for index, autonomous_driving in enumerate(self.autonomous_drivings):
-            self.movement_histories[index].append(
-                autonomous_driving.move(
+
+        for car_info in self.cars_info:
+            car_info["movement_history"].append(
+                car_info["autonomous_driving"].move(
                     [
-                        self.autonomous_drivings[carIndex].car_simulation
-                        for carIndex in self.preferences_cars[index]
+                        car_with_preference["autonomous_driving"].car_simulation
+                        for car_with_preference in car_info["cars_with_preference"]
                     ]
                 )
             )
 
     def save_cars_movement(self):
-        for index in range(len(self.cars)):
-            if not os.path.exists(SAVED_CAR_MOVEMENT_DIRECTORY):
-                os.makedirs(SAVED_CAR_MOVEMENT_DIRECTORY)
+        for car_info in self.cars_info:
+            if not os.path.exists(general_car_movement_directory):
+                os.makedirs(general_car_movement_directory)
             intersection_directory = os.path.join(
-                SAVED_CAR_MOVEMENT_DIRECTORY, self.saved_car_movement_directory
+                general_car_movement_directory, intersection_car_movement_directory
             )
             if not os.path.exists(intersection_directory):
                 os.makedirs(intersection_directory)
@@ -74,11 +84,12 @@ class Intersection:
             )
             if not os.path.exists(intersection_manoeuvre_directory):
                 os.makedirs(intersection_manoeuvre_directory)
+            car_id = car_info["id"]
             file_path = os.path.join(
-                intersection_manoeuvre_directory, f"car{index+1}.txt"
+                intersection_manoeuvre_directory, f"car{car_id}.txt"
             )
             with open(file_path, "w") as file:
-                for mod in self.movement_histories[index]:
+                for mod in car_info["movement_history"]:
                     file.write(f"{mod[0].name} {mod[1].name}\n")
 
     def add_car(
@@ -95,30 +106,31 @@ class Intersection:
             )
         )
         car = Car(brand, start_position, direction)
-        self.cars.append(car)
-        self.starting_site.append(direction_start)
-        self.ending_site.append(direction_end)
-        self.movement_histories.append([])
-        # calculate non preference zone
         manoeuvre = Manoeuvre(
             [IntersectionManoeuvre(track_points, non_preference_zone)]
         )
-        autonomous_driving = BasicAutonomousDriving(car, manoeuvre)
-        self.autonomous_drivings.append(autonomous_driving)
-        self.calculate_preferences()
-        if self.read_movement_from_file:
-            file_path = os.path.join(
-                SAVED_CAR_MOVEMENT_DIRECTORY,
-                self.saved_car_movement_directory,
-                self.directory_name,
-                f"car{len(self.cars)}.txt",
-            )
-            with open(file_path, "r") as file:
-                self.steps.append([tuple(line.split()) for line in file])
 
-    def calculate_preferences(self):
-        starting_site = self.starting_site[-1]
-        ending_site = self.ending_site[-1]
+        car_info = {
+            "id": len(self.cars_info) + 1,
+            "car": car,
+            "starting_site": direction_start,
+            "ending_site": direction_end,
+            "movement_history": [],
+            "autonomous_driving": BasicAutonomousDriving(car, manoeuvre),
+            "sites_with_preference": self.calculate_sites_with_preference(
+                direction_start, direction_end
+            ),
+            "cars_with_preference": [],
+        }
+        # calculate non preference zone
+
+        if self.read_movement_from_file:
+            car_info["movement_history"] = self.get_car_movement(car_info["id"])
+
+        self.cars_info.append(car_info)
+        self.calculate_cars_with_preference(car_info)
+
+    def calculate_sites_with_preference(self, starting_site, ending_site):
         directions = [Directions.DOWN, Directions.RIGHT, Directions.UP, Directions.LEFT]
         starting_site_index = directions.index(starting_site)
         preferences_directions = []
@@ -127,15 +139,35 @@ class Intersection:
                 preferences_directions.append(directions[(starting_site_index + i) % 4])
             else:
                 break
-        self.preferences_directions.append(preferences_directions)
-        last_index = len(self.cars) - 1
-        for index, preference_list in enumerate(self.preferences_cars):
-            if starting_site in self.preferences_directions[index]:
-                preference_list.append(last_index)
-        self.preferences_cars.append(
-            [
-                index
-                for index in range(last_index)
-                if self.starting_site[index] in preferences_directions
-            ]
+        return preferences_directions
+
+    def calculate_cars_with_preference(self, car_info: IntersectionCarInfo):
+        starting_site = car_info["starting_site"]
+
+        # update cars_with_preference for other cars if they have to give preference to car_info
+        for intersection_car in self.cars_info:
+            if (
+                intersection_car is not car_info
+                and starting_site in intersection_car["sites_with_preference"]
+            ):
+                intersection_car["cars_with_preference"].append(car_info)
+
+        # set cars_with_preference for car_info
+        car_info["cars_with_preference"] = [
+            intersection_car
+            for intersection_car in self.cars_info
+            if intersection_car is not car_info
+            and intersection_car["starting_site"] in car_info["sites_with_preference"]
+        ]
+
+    def get_car_movement(self, car_id):
+        file_path = os.path.join(
+            general_car_movement_directory,
+            intersection_car_movement_directory,
+            self.directory_name,
+            f"car{car_id}.txt",
         )
+        movement_history = []
+        with open(file_path, "r") as file:
+            movement_history.extend([tuple(line.split()) for line in file])
+        return movement_history
