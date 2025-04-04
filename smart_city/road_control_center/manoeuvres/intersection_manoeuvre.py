e228dd39d5f2bb1e4cb4b43008eaa741a9746f74
PawelTkocz <pawel.tkocz@vp.pl>
2025-04-04 03:55:28 +0200

    store data about each segment on track

--- smart_city/road_control_center/manoeuvres/intersection_manoeuvre.py
+++ smart_city/road_control_center/manoeuvres/intersection_manoeuvre.py
@@ -1,21 +1,18 @@
 from car.model import CarModel
 from geometry import Directions
 from road_segments.intersection.intersection import Intersection
+from schemas import CardinalDirection, HorizontalDirection, VerticalDirection
+
 from smart_city.road_control_center.manoeuvres.manoeuvre import Manoeuvre
 from smart_city.road_control_center.manoeuvres.manoeuvre_phase import ManoeuvrePhase
+from smart_city.road_control_center.manoeuvres.right_angle_turn import RightAngleTurn
 from smart_city.road_control_center.manoeuvres.schemas import (
     IntersectionManoeuvreDescription,
 )
-from smart_city.road_control_center.manoeuvres.track import (
-    TrackPath,
-    get_right_angle_turn,
-    get_straight_track,
-)
+from smart_city.road_control_center.manoeuvres.straight_path import StraightPath
+from smart_city.road_control_center.manoeuvres.track import ManoeuvreTrack
 
-vertical = [Directions.UP, Directions.DOWN]
-horizontal = [Directions.LEFT, Directions.RIGHT]
-directions = [Directions.UP, Directions.RIGHT, Directions.DOWN, Directions.LEFT]
-TURN_SHARPNESS = 0.7
+EXPECTED_MIN_TURN_VELOCITY = 2
 
 
 def calculate_turn_track(
@@ -24,8 +21,7 @@ def calculate_turn_track(
     car_length: float,
     turn_direction: Directions,
     turn_margin: float,
-    turn_sharpness: float,
-) -> TrackPath:
+) -> ManoeuvreTrack:
     starting_side = manoeuvre_description["starting_side"]
     ending_side = manoeuvre_description["ending_side"]
     incoming_line = intersection.intersection_parts["incoming_lines"][starting_side]
@@ -40,82 +36,98 @@ def calculate_turn_track(
     turn_end_point = outcoming_line.rear_middle.add_vector(
         outcoming_line.direction.scale_to_len(turn_margin)
     )
-    incoming_track_path = get_straight_track(start_point, turn_start_point)
-    turning_track_path = get_right_angle_turn(
-        turn_start_point, turn_end_point, turn_direction, turn_sharpness
+    return ManoeuvreTrack(
+        [
+            {
+                "track_segment": StraightPath(start_point, turn_start_point),
+                "expected_min_velocity": None,
+            },
+            {
+                "track_segment": RightAngleTurn(
+                    turn_start_point, turn_end_point, turn_direction
+                ),
+                "expected_min_velocity": EXPECTED_MIN_TURN_VELOCITY,
+            },
+            {
+                "track_segment": StraightPath(turn_end_point, end_point),
+                "expected_min_velocity": None,
+            },
+        ]
     )
-    outcoming_track_path = get_straight_track(turn_end_point, end_point)
-    return incoming_track_path + turning_track_path + outcoming_track_path
 
 
-def calculate_left_turn_track_path(
+def calculate_left_turn_track(
     intersection: Intersection,
     manoeuvre_description: IntersectionManoeuvreDescription,
     car_length: float,
-) -> TrackPath:
+) -> ManoeuvreTrack:
     return calculate_turn_track(
         intersection,
         manoeuvre_description,
         car_length,
-        Directions.LEFT,
+        HorizontalDirection.LEFT,
         0,
-        TURN_SHARPNESS,
     )
 
 
-def calculate_right_turn_track_path(
+def calculate_right_turn_track(
     intersection: Intersection,
     manoeuvre_description: IntersectionManoeuvreDescription,
     car_length: float,
-) -> TrackPath:
+) -> ManoeuvreTrack:
     return calculate_turn_track(
         intersection,
         manoeuvre_description,
         car_length,
-        Directions.RIGHT,
+        HorizontalDirection.RIGHT,
         car_length,
-        TURN_SHARPNESS,
     )
 
 
-def calculate_straight_track_path(
+def calculate_straight_track(
     intersection: Intersection,
     manoeuvre_description: IntersectionManoeuvreDescription,
     car_length: float,
-) -> TrackPath:
+) -> ManoeuvreTrack:
     starting_side = manoeuvre_description["starting_side"]
     ending_side = manoeuvre_description["ending_side"]
     start_point = intersection.intersection_parts["incoming_lines"][
         starting_side
     ].rear_middle
-    outcoming_line = intersection.intersection_parts["outcoming_lines"][ending_side]
-    end_point = outcoming_line.front_middle.add_vector(
-        outcoming_line.direction.scale_to_len(2 * car_length)
+    outcoming_lane = intersection.intersection_parts["outcoming_lines"][ending_side]
+    end_point = outcoming_lane.front_middle.add_vector(
+        outcoming_lane.direction.scale_to_len(2 * car_length)
     )
-    return get_straight_track(start_point, end_point)
+    return ManoeuvreTrack(
+        [
+            {
+                "track_segment": StraightPath(start_point, end_point),
+                "expected_min_velocity": None,
+            }
+        ]
+    )
 
 
-def calculate_track_path(
+def calculate_track(
     intersection: Intersection,
     manoeuvre_description: IntersectionManoeuvreDescription,
     car_length: float,
-) -> TrackPath:
+) -> ManoeuvreTrack:
     starting_side = manoeuvre_description["starting_side"]
     ending_side = manoeuvre_description["ending_side"]
-    if (starting_side in vertical and ending_side in vertical) or (
-        starting_side in horizontal and ending_side in horizontal
+    if (starting_side in VerticalDirection and ending_side in VerticalDirection) or (
+        starting_side in HorizontalDirection and ending_side in HorizontalDirection
     ):
-        return calculate_straight_track_path(
-            intersection, manoeuvre_description, car_length
-        )
+        return calculate_straight_track(intersection, manoeuvre_description, car_length)
 
+    directions = list(CardinalDirection)
     starting_side_index = directions.index(starting_side)
     if directions[(starting_side_index + 1) % 4] == ending_side:
-        return calculate_left_turn_track_path(
+        return calculate_left_turn_track(
             intersection, manoeuvre_description, car_length
         )
     else:
-        return calculate_right_turn_track_path(
+        return calculate_right_turn_track(
             intersection, manoeuvre_description, car_length
         )
 
@@ -132,7 +144,7 @@ class IntersectionManoeuvre(Manoeuvre):
         super().__init__(
             [
                 ManoeuvrePhase(
-                    calculate_track_path(
+                    calculate_track(
                         intersection, manoeuvre_description, model.appearance["length"]
                     )
                 )
