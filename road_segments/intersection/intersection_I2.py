3c1cc92ac0634df9a1982376b75f00bf2b34c1dc
PawelTkocz <pawel.tkocz@vp.pl>
2025-04-25 11:02:58 +0200

    introduce traffic_control_element class

--- road_segments/intersection/intersection_I2.py
+++ road_segments/intersection/intersection_I2.py
@@ -1,12 +1,12 @@
 from road_segments.intersection.intersection import Intersection
 from schemas import CardinalDirection
+from traffic_control_elements.traffic_control_element import TrafficControlElement
 from traffic_control_elements.traffic_signs.signs import SignB20, SignD1
-from traffic_control_elements.traffic_signs.traffic_sign import TrafficSign
 
 
 class IntersectionI2(Intersection):
     def __init__(self) -> None:
-        control_elements: dict[CardinalDirection, list[TrafficSign]] = {
+        control_elements: dict[CardinalDirection, list[TrafficControlElement]] = {
             CardinalDirection.DOWN: [SignB20()],
             CardinalDirection.LEFT: [SignD1()],
             CardinalDirection.RIGHT: [SignD1()],
--- traffic_control_elements/traffic_signs/signs.py
+++ traffic_control_elements/traffic_signs/signs.py
@@ -7,45 +7,51 @@ from traffic_control_elements.traffic_signs.traffic_sign import (
 
 class SignA5(TrafficSign):
     def __init__(self) -> None:
-        super().__init__(TrafficSignName.A5, "sign_A5")
+        super().__init__(TrafficSignName.A5, "sign_A5.png")
 
 
 class SignD1(TrafficSign):
     def __init__(self) -> None:
-        super().__init__(TrafficSignName.D1, "sign_D1")
+        super().__init__(TrafficSignName.D1, "sign_D1.png")
 
 
 class SignA7(TrafficSign):
     def __init__(self) -> None:
-        super().__init__(TrafficSignName.A7, "sign_A7")
+        super().__init__(TrafficSignName.A7, "sign_A7.png")
 
 
 class SignB20(TrafficSign):
     def __init__(self) -> None:
-        super().__init__(TrafficSignName.B20, "sign_B20")
+        super().__init__(TrafficSignName.B20, "sign_B20.png")
 
 
 class SignT6aLeft(TrafficSign):
     def __init__(self) -> None:
-        super().__init__(TrafficSignName.T6a_LEFT, "sign_T6a")
+        super().__init__(TrafficSignName.T6a_LEFT, "sign_T6a.png")
 
 
 class SignT6aRight(TrafficSign):
     def __init__(self) -> None:
-        super().__init__(TrafficSignName.T6a_RIGHT, "sign_T6a")
-        self.original_image = pygame.transform.rotate(self.original_image, 90)
-        self.directed_image = self.original_image
+        super().__init__(TrafficSignName.T6a_RIGHT, "sign_T6a.png")
+        self.original_state_images = [
+            pygame.transform.rotate(self.original_state_images[0], 90)
+        ]
+        self.rotated_state_images = self.original_state_images
 
 
 class SignT6cLeft(TrafficSign):
     def __init__(self) -> None:
-        super().__init__(TrafficSignName.T6c_LEFT, "sign_T6a")
-        self.original_image = pygame.transform.rotate(self.original_image, 180)
-        self.directed_image = self.original_image
+        super().__init__(TrafficSignName.T6c_LEFT, "sign_T6a.png")
+        self.original_state_images = [
+            pygame.transform.rotate(self.original_state_images[0], 180)
+        ]
+        self.rotated_state_images = self.original_state_images
 
 
 class SignT6cRight(TrafficSign):
     def __init__(self) -> None:
-        super().__init__(TrafficSignName.T6c_RIGHT, "sign_T6a")
-        self.original_image = pygame.transform.rotate(self.original_image, 270)
-        self.directed_image = self.original_image
+        super().__init__(TrafficSignName.T6c_RIGHT, "sign_T6a.png")
+        self.original_state_images = [
+            pygame.transform.rotate(self.original_state_images[0], 270)
+        ]
+        self.rotated_state_images = self.original_state_images
