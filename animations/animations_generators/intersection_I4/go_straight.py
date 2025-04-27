69c2311d1b1aa177f1d5c9d9f996be1524550e67
PawelTkocz <pawel.tkocz@vp.pl>
2025-04-28 01:10:27 +0200

    animations for intersection i4

--- animations/animations_generators/intersection_I4/go_straight.py
+++ animations/animations_generators/intersection_I4/go_straight.py
@@ -27,7 +27,7 @@ CARS_DESCRIPTIONS: list[IntersectionAnimationCarDescription] = [
             "ending_side": CardinalDirection.RIGHT,
         },
         "model": TOYOTA_YARIS_SPECIFICATION,
-        "start_frame_number": 0,
+        "start_frame_number": 10,
         "velocity": 0,
     },
     {
@@ -35,10 +35,10 @@ CARS_DESCRIPTIONS: list[IntersectionAnimationCarDescription] = [
         "color": "purple",
         "manoeuvre_description": {
             "starting_side": CardinalDirection.RIGHT,
-            "ending_side": CardinalDirection.DOWN,
+            "ending_side": CardinalDirection.UP,
         },
         "model": TOYOTA_YARIS_SPECIFICATION,
-        "start_frame_number": 0,
+        "start_frame_number": 10,
         "velocity": 0,
     },
 ]
--- animations/animations_generators/intersection_I4/turn_left.py
+++ animations/animations_generators/intersection_I4/turn_left.py
@@ -12,11 +12,11 @@ CARS_DESCRIPTIONS: list[IntersectionAnimationCarDescription] = [
         "registry_number": "DW001",
         "color": "red",
         "manoeuvre_description": {
-            "starting_side": CardinalDirection.RIGHT,
-            "ending_side": CardinalDirection.UP,
+            "starting_side": CardinalDirection.UP,
+            "ending_side": CardinalDirection.DOWN,
         },
         "model": TOYOTA_YARIS_SPECIFICATION,
-        "start_frame_number": 0,
+        "start_frame_number": 10,
         "velocity": 0,
     },
     {
@@ -38,7 +38,7 @@ CARS_DESCRIPTIONS: list[IntersectionAnimationCarDescription] = [
             "ending_side": CardinalDirection.UP,
         },
         "model": TOYOTA_YARIS_SPECIFICATION,
-        "start_frame_number": 0,
+        "start_frame_number": 10,
         "velocity": 0,
     },
 ]
